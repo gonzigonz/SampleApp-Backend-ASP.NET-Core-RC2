ac6ea5220dd96df50b2f045e314f50ef48b02f5f
Gonzalo Lucero <gonzigonz@users.noreply.github.com>
2016-05-18 18:44:51 +1000

    Added POST, UPDATE & DELETE methods to "Todo" API endpoint

--- Gonzigonz.SampleApp/WebAPI/Controllers/TodoController.cs
+++ Gonzigonz.SampleApp/WebAPI/Controllers/TodoController.cs
@@ -1,4 +1,5 @@
-﻿using Gonzigonz.SampleApp.RepositoryInterfaces;
+﻿using Gonzigonz.SampleApp.Domain;
+using Gonzigonz.SampleApp.RepositoryInterfaces;
 using Microsoft.AspNetCore.Mvc;
 using System.Net;
 
@@ -27,9 +28,58 @@ namespace WebAPI.Controllers
 			if (item == null)
 			{
 				Response.StatusCode = (int)HttpStatusCode.NotFound;
-				return Json(null);
+				return Json($"No item was found with an id of {id}");
 			}
 			return Json(item);
 		}
+
+		[HttpPost("")]
+		public JsonResult Post([FromBody] TodoItem newTodoItem)
+		{
+			if (newTodoItem == null)
+			{
+				Response.StatusCode = (int)HttpStatusCode.BadRequest;
+				return Json("No item was passed to create");
+			}
+			else
+			{
+				Response.StatusCode = (int)HttpStatusCode.Created;
+				Response.Headers["Location"] = $"/api/Todo/{newTodoItem.Id}"; //TODO: Gonz do this properly... all api items should provide valid urls.
+				return Json(_todoRepo.Create(newTodoItem));
+			}
+		}
+
+		[HttpPut("{id}")]
+		public JsonResult Put(int id, [FromBody] TodoItem updatedTodoItem)
+		{
+			if (updatedTodoItem == null || updatedTodoItem.Id != id)
+			{
+				Response.StatusCode = (int)HttpStatusCode.BadRequest;
+				return Json("Invalid item was passed or the item id does not match the id supplied in the url");
+			}
+
+			var todo = _todoRepo.ReadById(id);
+			if (todo == null)
+			{
+				Response.StatusCode = (int)HttpStatusCode.NotFound;
+				return Json($"No item was found with an id of {id}");
+			}
+
+			Response.Headers["Location"] = $"/api/Todo/{updatedTodoItem.Id}"; //TODO: Gonz do this properly... all api items should provide valid urls.
+			return Json(_todoRepo.Update(updatedTodoItem));
+		}
+
+		[HttpDelete("{id}")]
+		public JsonResult Delete(int id)
+		{
+			var itemToDelete = _todoRepo.ReadById(id);
+			if (itemToDelete == null)
+			{
+				Response.StatusCode = (int)HttpStatusCode.NotFound;
+				return Json($"No item was found with an id of {id}");
+			}
+			_todoRepo.Delete(itemToDelete);
+			return Json($"The item with the id of {itemToDelete.Id} was deleted");
+		}	
 	}
 }

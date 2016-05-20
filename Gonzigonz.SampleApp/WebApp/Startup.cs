216ff55e6ab9f8f70588e5d8bb392f58080721ad
Gonzalo Lucero <gonzigonz@users.noreply.github.com>
2016-05-21 02:16:26 +1000

    Enabled StaticFiles and Mvc

--- Gonzigonz.SampleApp/WebApp/Startup.cs
+++ Gonzigonz.SampleApp/WebApp/Startup.cs
@@ -9,21 +9,29 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace WebApp
 {
-    public class Startup
-    {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
-        {
-        }
+	public class Startup
+	{
+		// This method gets called by the runtime. Use this method to add services to the container.
+		// For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+		public void ConfigureServices(IServiceCollection services)
+		{
+			// Inject MVC6 service
+			services.AddMvc();
+		}
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app)
-        {
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
-        }
-    }
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app)
+		{
+			// Allows any static content in wwwroot to be servered
+			app.UseStaticFiles();
+
+			// Config MVC6
+			app.UseMvc(config =>
+			{
+				config.MapRoute(
+					name: "default",
+					template: "{controller=App}/{action=Index}/{id?}");
+			});
+		}
+	}
 }

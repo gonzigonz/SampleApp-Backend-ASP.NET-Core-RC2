ca39faceb85daaa94f78418c7b0dc93d6489f048
Gonzalo Lucero <gonzigonz@users.noreply.github.com>
2016-05-22 18:56:44 +1000

    Added new AppController and Index to WebApp project.
     - Index pages retrieves the list of Todo items.

---
+++ Gonzigonz.SampleApp/WebApp/Controllers/Web/AppController.cs
@@ -0,0 +1,23 @@
+﻿using Gonzigonz.SampleApp.Data.Context;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace WebApp.Controllers.Web
+{
+	public class AppController : Controller
+    {
+		private AppDbContext _context;
+
+		public AppController(AppDbContext context)
+		{
+			_context = context;
+		}
+
+		public async Task<IActionResult> Index()
+		{
+			var todoItems = await _context.TodoItems.ToListAsync();
+			return View(todoItems);
+		}
+	}
+}

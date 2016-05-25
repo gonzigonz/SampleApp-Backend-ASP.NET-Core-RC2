bbb02c952c8eddbe90951fa63021d48714705de5
Gonzalo Lucero <gonzigonz@users.noreply.github.com>
2016-05-25 11:19:47 +1000

    Added Automatic Audi Info Update for all "EntityBase" models
     - Change to AppUnitOfWork, to update the audit columns of every model inherited from "EntityBase" class, whenever "SaveChangesAsync" is called.

--- Gonzigonz.SampleApp/Gonzigonz.SampleApp.Data/UnitOfWork/AppUnitOfWork.cs
+++ Gonzigonz.SampleApp/Gonzigonz.SampleApp.Data/UnitOfWork/AppUnitOfWork.cs
@@ -1,10 +1,14 @@
 ﻿using Gonzigonz.SampleApp.Data.Context;
+using Gonzigonz.SampleApp.Domain;
 using Gonzigonz.SampleApp.RepositoryInterfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
 
 namespace Gonzigonz.SampleApp.Data.UnitOfWork
 {
 	public class AppUnitOfWork : IUnitOfWork
-    {
+	{
 		private AppDbContext _context;
 
 		public AppUnitOfWork(AppDbContext context)
@@ -14,6 +18,20 @@ namespace Gonzigonz.SampleApp.Data.UnitOfWork
 
 		public void SaveChangesAsync()
 		{
+			foreach (var entry in _context.ChangeTracker.Entries()
+					   .Where(
+					   e => e.Entity is EntityBase &&
+					   (e.State == EntityState.Added) ||
+					   (e.State == EntityState.Modified))
+			)
+			{
+				var e = (EntityBase)entry.Entity;
+				if (entry.State == EntityState.Added)
+				{
+					e.CreatedTimeUTC = DateTime.UtcNow;
+				}
+				e.ModifiedTimeUTC = DateTime.UtcNow;
+			}
 			_context.SaveChangesAsync();
 		}
 	}
